42bbbc7f7eceb17a814e72256f2756188ce53d9e
António Paulino <antonioppaulino2@gmail.com>
2025-05-09 22:26:10 +0100

    Server initial files

---
+++ server/src/main/java/landmarkDetection/Server.java
@@ -0,0 +1,20 @@
+package landmarkDetection;
+
+public class Server {
+
+    private static int svcPort = 8000;
+
+    public static void main(String[] args) {
+        try {
+            if (args.length > 0) svcPort = Integer.parseInt(args[0]);
+
+            System.out.println("Starting server on port " + svcPort);
+            // Runtime.getRuntime().addShutdownHook(new ShutdownHook(svc));
+
+            // svc.awaitTermination();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+        }
+    }
+}
