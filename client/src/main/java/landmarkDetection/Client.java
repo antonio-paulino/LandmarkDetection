cc88b1d7fdc4c0cb0fafa97a569765adc18646d7
António Paulino <antonioppaulino2@gmail.com>
2025-05-09 22:25:38 +0100

    Client initial files

---
+++ client/src/main/java/landmarkDetection/Client.java
@@ -0,0 +1,16 @@
+package landmarkDetection;
+
+public class Client {
+    public static void main(String[] args) {
+        // Create a new instance of the Client class
+        Client client = new Client();
+
+        // Call the run method to start the client
+        client.run();
+    }
+
+    public void run() {
+        // Your code to run the client goes here
+        System.out.println("Client is running...");
+    }
+}
